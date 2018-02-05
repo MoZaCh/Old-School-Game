7eeed31d56263c25b72f85039956a969ffb78311
Aleksandras Asavkinas (asavkina) <asavkina@coventry.ac.uk>
2018-02-05 18:31:22 +0200

    Create EnemyEncounter.cpp

---
+++ EnemyEncounter.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "EnemyEncounter.h"
+
+
+EnemyEncounter::EnemyEncounter()
+{
+}
+
+
+EnemyEncounter::~EnemyEncounter()
+{
+}
+
+void EnemyEncounter::encounter(Character &character)
+{
+	//Enemy encounter event
+
+}
