4831c23a85869881439fb842b611e7a88a429b24
Harshkumar Patel (patelh28) <patelh28@coventry.ac.uk>
2018-02-14 19:20:53 +0000

    Update Class.cpp

--- Class.cpp
+++ Class.cpp
@@ -6,7 +6,7 @@ class Character
     void message()
     {
     string choice;
-    cout << "Please choose a character"<<endl<<"Mage"<<endl<<"Priest"<<endl<<"Warrior"<<endl<<"Necromancer"<<endl<<"Hunter"<<endl;
+    cout << "Please choose a character"<<endl<<"1. Mage"<<endl<<"2. Priest"<<endl<<"3. Warrior"<<endl<<"4. Necromancer"<<endl<<"5. Hunter"<<endl;
     cin >> choice;  
     }
   protected :
@@ -18,34 +18,97 @@ class Mage : public Character
 {
  
 public :
-    
+    void abilities()
+    {
+    attack = 70;
+    defend = 30;
+    healthpoints = 20;
+    }
     void SpellBook(){}
     void Staff(){}
+    void Blink(){}
+    void CastSpell(){}
+    void FIREBALL(){}
+    void LIGHTNING(){}
+    void FREEZE(){}
+    void TORNADO(){}
+    
+    
 };    
 
 class Priest : public Character
 {
 public :
-    void Talisman(){} 
+    void abilities()
+    {
+    attack = 50;
+    defend = 50;
+    healthpoints = 50;
+    }
+    void Talisman(){}
+    void Levitate(){}
+    void Heal(){}
+    void Spell (){}
+    
 };
 class Warrior : public Character
 {
 public :
+    void abilities()
+    {
+    attack = 80;
+    defend = 50;
+    healthpoints = 60;
+    }
     void GreatSword(){}
     void Axe(){}
-    
-      
+    void Stun(){}
+    void Swing(){}
+    void Block(){}
+    void Guard(){}
+    void Charge(){}
+    void BladeStorm(){}
 };
+
 class  Necromancer : public Character
 {
 public :
+    void abilities()
+    {
+    attack = 100;
+    defend = 80;
+    healthpoints = 70;
+    }
     void Staff (){}
+    void Summon (){}
+    void CastSpell (){}
+    void SummonGhoul (){}
+    void LifeDrain (){}
+    void Corrupt (){}
+    void Fear (){}
+    void Burn (){}
+    void Buffedfromselfdamage (){}
      
 };
 class  Hunter : public Character
 {
 public :
+void abilities()
+    {
+    attack = 100;
+    defend = 100;
+    healthpoints = 100;
+    }
     void Bow (){}
+    void Dagger (){}
+    void Shoot (){}
+    void Climb (){}
+    void Snipe (){}
+    void Call (){}
+    void Assassinate (){}
+    void RainofArrors(){}
+    void ThrowDagger (){}
+    void SetTraps(){}
 };
 
 
@@ -56,3 +119,4 @@ int main()
   C1.message();
   return 0;
 }
+
