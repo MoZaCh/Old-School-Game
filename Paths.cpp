a1ce9232f84cce5376e68cecb7d3b67c9829590c
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2018-02-16 14:57:03 +0000

    Update Paths.cpp

--- Paths.cpp
+++ Paths.cpp
@@ -49,7 +49,8 @@ cout << "You seem to not be able to remember much but you do gather your name." 
     
     else 
     {
-        cout << "Must be a Y/N answer!" << endl; //Error Check
+        cout << endl;
+        cout << "*Invalid Input! Must be a Y/N answer*" << endl; //Error Check
         // sleep
         cout << endl; //New Line
         goto confirmCheck; // Loops Back
@@ -93,7 +94,7 @@ cout << "You seem to not be able to remember much but you do gather your name." 
        if(caveStay == "Yes" || caveStay == "yes") //confirming answer
        {
            cout << endl;
-           cout << "You keep going, you keep taking a step further into the darkness.\nAt this point you are completely shrouded in darkness, you aren't able to see a single thing.\nYou aren't able to see anything, But you continue to venture onward, you end up hitting a wall.\nYou're able to feel a breeze on the other side and are even able to see a crack of light behind the rocks." << endl;
+           cout << "You keep going, you keep taking a step further into the darkness.\nAt this point you are completely shrouded in darkness, you aren't able to see a single thing,\nBut you continue to venture onward, you end up hitting a wall.\nYou're able to feel a breeze on the other side and are even able to see a crack of light behind the rocks." << endl;
            cout << endl;
            cout << "You try to chip away at the rocks, finding it with ease like these rocks have been moved a lot.\nyou discover something hidden behind them, it seems to be some type of small room, lit up by just one torch.\nthere is a pedestal, there seems to be a weapon ontop, \nits a *DEPENDS ON CLASS*" << endl;
            cout << endl;
@@ -101,7 +102,7 @@ cout << "You seem to not be able to remember much but you do gather your name." 
            cout << endl;
            cout << "Do you wish to take the weapon?" << endl;
            
-           TakeWeapon:
+           TakeWeapon://Loop Thingy
            
            cout << endl;
            
@@ -113,9 +114,40 @@ cout << "You seem to not be able to remember much but you do gather your name." 
            {
             cout << "You take the weapon carefully still being cautious about your surroundings expecting a trap of some sort.\nYou now have the weapon in your hands and take a step back from the pedestal,\nnothing seems to happen" << endl;
             cout << endl;
-            cout << "It was too soon for you to relax for the enterace to the room shuts closed. you seem to now be trapped in the room. \nYou start to panic. You start to use the weapon that you have just picked up without knowing what it would do and \nstart swinging at the walls with it, its creates a huge dent in the wall with ease but not enough to get out.\nThe impact was strong enough to blow the torch out." << endl;
+            cout << "It was too soon for you to relax for the entrance to the room shuts closed. you seem to now be trapped in the room. \nYou start to panic. You start to use the weapon that you have just picked up without knowing what it would do and \nstart swinging at the walls with it, its creates a huge dent in the wall with ease but not enough to get out.\nThe impact was strong enough to blow the torch out." << endl;
             cout << endl;
             cout << "The torch goes out, Its now complete darkness in this small room." << endl;
+               cout << endl;
+               cout << "You start to feel something drippling onto your shoulder, you can tell from the texture that it is not water.\n\nIts Oil!\n\nYou try to feel around the room again to see if you can find some type of escape\nbefore the room fills with oil.\nThe oil is rushing in quicker by the second,\nits still completely dark in the room you're able to find the gap that the oil is coming from. \nYou try to squeeze through the gap of the oil before the oil \ncompletely fills the room, suffocating you but your just not able to fit.\n\nYou have no other choice but to use the weapon that you have to try and make the hole wider, you've already seen the force of the weapon when you tried to escape before." << endl;
+               
+               Death://Loop Thingy
+               cout << endl;
+               string death;
+               
+               cout << "Do you ready or Swing or Accept your impending doom." << endl;
+               cin >> death;//Input
+               
+               if (death == "Swing" || death == "swing")
+               {
+                   cout << endl;
+                   cout << "You ready your swing, closing your eyes hoping for the best and for the fact that if you didnt you would get oil in them.\n\nYou swing.\n\nThe worst has happened. You never expected the fact that a spark could have been made from the collision of impact.\n\nIt has ignited the oil, burning you to a crisp." << endl;
+                   cout << "GAME OVER" << endl;
+                   return 0;//End Code
+               }
+               else if (death == "Accept" || death == "accept")
+               {
+                   cout << endl;
+                   cout << "You close your eyes accepting your fate as the room fills with oil.\nAll you can hear is the sound of the oil rushing in at a quicker pace that before.\n\nThe oil has almost completely filled up the room.\n\nYou end up getting suffocated by the oil, gasping for air." << endl;
+                   cout << "GAME OVER" << endl;
+                   return 0;//End Code
+               }
+               else{
+                   cout << endl;
+                   cout << "*Invalid input! Must answer with Swing or Accept*" << endl;
+                   death.clear();
+                   cout << endl;
+                   goto Death;
+               }
            }
            else if (takeWeapon == "Leave" || takeWeapon == "leave")
            {
@@ -124,11 +156,11 @@ cout << "You seem to not be able to remember much but you do gather your name." 
            else
            {
                cout << "*Invalid input! Must answer with Take or Leave*" << endl;
-               takeWeapon.clear();
-               goto TakeWeapon;
+               takeWeapon.clear();//Clear Variable
+               goto TakeWeapon;//Loop back
                cout << endl;
            }
-           return 0; //End Code
+           
        }
        else if (caveStay == "No" || caveStay == "no")
        {
@@ -137,10 +169,10 @@ cout << "You seem to not be able to remember much but you do gather your name." 
        else 
        {
            cout << endl;
-           cout << "not a valid option" << endl;
+           cout << "*Invalid Input! Must answer with Yes or No*" << endl;
            caveStay.clear(); //clears variable
            cout << endl;
-           goto CaveStay; //loops bacl
+           goto CaveStay; //loops back
        }
        
    }
@@ -152,11 +184,15 @@ cout << "You seem to not be able to remember much but you do gather your name." 
     else 
     {
         cout << endl;
-        cout << "not a valid option" << endl; //Error
+        cout << "*Invalid Input! Must answer with Stay or Leave*" << endl; //Error
         cout << endl;
         Cave.clear(); //Clearing Variable
         goto cave; //Loop
     }
+    
+    
+    
+    
     
     cout << "Test" << endl; // TEST!
     
