8233c528c154f83d9d5d4896af2c55727dd06674
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2018-02-22 23:54:05 +0000

    A Different Version of Menu
    Was a problem with the other version, would cause an infinite loop if the user entered anything but a number.

---
+++ MainMenuV2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cstdlib>
+#include "Paths.cpp"
+using namespace std;
+
+int main()
+{ 
+
+  title:
+  cout <<"\n===========================================\n"<<
+          "T i t l e\n"<<
+          "===========================================\n"<<endl;
+  
+  cout<< "1) Start New Game\n"<<
+         "2) Resume Game\n"<<
+         "3) Credits\n"<<
+         "4) Exit"<<endl;
+  cout<< "\nSelect an option: ";
+  string choice;
+      option:
+  cin>> choice;
+    
+  if (choice == "1") //Start New Game
+  {
+          system("clear");
+          path();
+  }
+  
+  else if (choice == "2") // Resume Game
+  {
+      
+  }
+  
+  else if (choice == "3")
+  {
+      choice.clear();
+      
+  string choice; 
+      
+ system("clear");
+      
+    Choice:
+      
+  cout<<"Credits:\n"<<
+        "------------------------------------ \n"<<
+        "*KHAN Qais* \n"<<
+        "*LAWAL Paul* \n"<<
+        "*ASAVKINAS Aleksandras* \n"<<
+        "*MAREK Peter* \n"<<
+        "*CHOUDHURY Mohammed Zahed* \n"<<
+        "*PATEL Harshkumar Chandrakant* \n"<<
+        "------------------------------------"<<endl;
+  cout<< "Would you like to go back? (Y/N): ";
+  cin>>choice;
+  
+  if (choice=="Y" || choice=="y")
+  {
+    system("clear");
+      goto title;
+  }
+      
+  else if (choice == "N" || choice == "n")
+  {
+      system("clear");
+    exit(0);
+  }
+      
+    else
+    {
+        cout << "Invalid Input! Must Answer with Y or N" << endl;
+       goto Choice; 
+    }
+      
+  }
+  
+  else if (choice == "4")
+  {
+          system("clear");
+  return 0;
+  }
+  
+  else 
+  {
+  cout<< "Wrong choice, try again: ";
+     choice.clear();
+    goto option;
+  }
+  return 0;
+}
