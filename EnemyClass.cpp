cae6132101bdfd6e7b8c488a67376e10eaf0bbd3
Peter Marek (marekp) <marekp@coventry.ac.uk>
2018-03-05 23:12:11 +0000

    Enemy Class for Combat

---
+++ EnemyClass.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Enemy {
+private:
+	int health;
+	int attack;
+	int defence;
+	string weapon;
+public:
+	//getters
+	int getEnemyHealth() { return health; }
+	int getEnemyAttack() { return attack; }
+	int getEnemyDefence() { return defence; }
+	string getEnemyWeapon() { return weapon; }
+   
+
+	Enemy(int, int, int, string);
+
+	void toEnemyString() {
+		cout << "Health: " << this->getEnemyHealth() 
+		 << " Attack: " << this->getEnemyAttack() 
+		 << " Defence: " << this->getEnemyDefence() 
+		 << " Weapon: " << this->getEnemyWeapon() << endl;
+
+	}
+};
+
+// A constructor is called when an object is created
+Enemy::Enemy(int health, int attack, int defence,
+	string weapon) {
+	// This is used to refer to an object created of this class type  
+	this->health = health;
+	this->attack = attack;
+	this->defence = defence;
+	this->weapon = weapon;
+}
+
+
+
+
+
