0587e7d7cc4673b53663ec8df818e6c2ae261bca
Mohammed Choudhury (choudh70) <choudh70@coventry.ac.uk>
2018-03-06 23:25:19 +0000

    Create sqlcon.cpp

---
+++ sqlcon.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <limits>
+#include "Paths.cpp"
+using namespace std;
+
+#include "libsqlite.hpp"
+
+//Insert into Function -- Name and Class
+void insertSQL(string username, string gameClass)
+{
+  sqlite::sqlite db("testdb.db");
+  auto cur = db.get_statement();
+  
+  cur->set_sql("INSERT INTO users (username, class)"
+               "VALUES (?, ?);"); //sql command
+  cur->prepare();
+  cur->bind(1, username);
+  cur->bind(2, gameClass);
+  cur->step();
+}
+
+
+bool nameCheck( string name, bool nameUsed)
+{
+  sqlite::sqlite db( "testdb.db" ); //Opens the connection
+  auto cur = db.get_statement(); //Creates a cursor on this connection
+  
+  cur->set_sql("select *"
+               "from users"); //sql command
+  cur->prepare(); //sends to database
+
+  
+  while(cur->step())
+  {
+    //cout << "ID: " << cur->get_int(0)<< " |Username: " <<  cur->get_text(1) << " |Class: " << cur->get_text(2) << endl;
+  
+    //int count = cur->get_int(0);
+    //cout << count << endl;
+    
+    
+    if (cur->get_text(1) == name)
+    {
+      nameUsed = true;
+      break;
+    }
+    
+    else
+    {
+      nameUsed = false;
+    }
+    
+  }
+  
+  if (nameUsed == true)
+  {
+    cout << "Username has already inuse" << endl;
+    nameUsed = true;
+    return nameUsed;
+  }
+  else
+  {
+    cout << "Welcome New User!!!!" << endl;
+    nameUsed = false;
+    return nameUsed;
+  }
+  return 0;
+}
+
+
+char chooseClass()
+{
+  char choice;
+
+	do{
+
+	cout << "Please choose a class type! \nMage (1) \nOption (2):"<< endl;
+	cin >> choice;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+  
+  while(choice != '1' && choice != '2' && choice != '3');
+  {
+    if (choice == '1')
+    {
+      cout << "You have chosen the Mage class" << endl;
+      return choice;
+    }
+    else if (choice == '2')
+    {
+      cout << "You have chosen the opt 2 class" << endl;
+      return choice;
+    }
+  }
+}
+
+
+void selectSQL()
+{
+  sqlite::sqlite db("testdb.db");
+  auto cur = db.get_statement();
+  
+  cur->set_sql("SELECT username)"
+               "FROM users"
+               "WHERE class = 'Mage';"); //sql command
+  cur->prepare();
+  //cur->bind(1, gen);
+  cur->step();
+  string count = cur->get_text(1);
+  cout << count << endl;
+}
+
+
+
+int sqlcon()
+{
+  
+  bool nameUsed = true;
+  while (nameUsed == true)
+  {
+    string name, gclass;
+    cout << "Enter a username: ";
+    cin >> name;
+    bool ans = nameCheck(name, nameUsed);
+    if (ans == false)
+    {
+      //cout << "Choose a class type! \nMage (1) \nOption (2)"<< endl;
+      //cin >> gclass;
+      //gclass = chooseClass();
+      //cout << gclass << endl;
+      break;
+    }
+          
+  }
+  path();
+}
