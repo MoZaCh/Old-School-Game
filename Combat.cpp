40708fd092fdc68f72ad2ef23e4a6a14faa2f192
Peter Marek (marekp) <marekp@coventry.ac.uk>
2018-03-08 21:07:02 +0000

    Done except database communication

--- Combat.cpp
+++ Combat.cpp
@@ -2,27 +2,52 @@
 #include <string>
 #include <unistd.h>
 #include "EnemyClass.cpp"
-#include "Main_Menu.cpp"
-#include "SpellClasses.cpp"
-
-//these two for random number function generator
-#include <cstdlib> 
-#include <ctime> 
+#include "MergedClasses.cpp"
+#include <random>
 
 
 using namespace std;
 
 int randomNumber(){                       
-  srand((unsigned)time(0)); 
-  int random_integer = rand(); 
-  return random_integer;
-  /* The following code will seed the generator with the system time and then output a single random number, which will be differen
-    t each time we run the program.In another words, the first line is neccesary for generating a different number, or rather different
-    sequence of numbers */
+    random_device rd;  //Will be used to obtain a seed for the random number engine
+    mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    uniform_int_distribution<> dis(1,100); // random numbers between 1-100
+    return dis(gen);
 }
+int mageDefeated(){
+          cout<<"You have been defeated by the enemy!"<<endl;
+          cout<<"Do you wish to go back to (some stage of the game)? (Y/N): ";
+          string choice2;
+          cin>>choice2;
+          if (choice2 == "y" || choice2 == "Y"){
+            system("clear");
+            cout<<"here will be function to jump somewhere in the game"<<endl;
+            }
+           else{
+            return 0;
+          } 
+}
+int enemyDefeated(){
+          cout<<"You have successfully killed your enemy!"<<endl;
+          cout<<"Do you wish to go back to (some stage of the game)? (Y/N): ";
+          string choice2;
+          cin>>choice2;
+          if (choice2 == "y" || choice2 == "Y"){
+            system("clear");
+            cout<<"here will be function to jump somewhere in the game"<<endl;
+            }
+          else{
+            return 0;
+          } 
+}
+
+void mageExecuted(){
+          cout<<"Your health was too low, enemy has executed you."<<endl;
+}
+  
 int main(){
- Mage mageObj(1200,1300,150,1500,"Staff");
-  Enemy enemyObj(2000,1500,500,"Axe");
+ Mage mageObj(1200,500,150,1500,"Staff");
+  Enemy enemyObj(2000,400,500,"Axe");
   
  cout<< "---------------You've entered battle---------------\n"<<endl;
  cout<< "YOUR STATS AND SPELLS: "<<endl;
@@ -31,80 +56,169 @@ int main(){
   
  cout<< "\nENEMY'S STATS AND ABILITIES: "<<endl;
  enemyObj.toEnemyString();
- cout<< "1) Execute  2) Mortal Strike  3) Charge  4) Reflect \n"<<endl;
+ enemyObj.abilities();
   
   
  //getting values from Mage object
  int mageHealth = mageObj.getHealth();
  int mageMana = mageObj.getMana();
- int mageAttack = mageObj.getAttack(); 
+ int mageAttack = mageObj.getAttack();
  int mageDeffence = mageObj.getDefence();
  //getting values from Enemy object
  int enemyHealth = enemyObj.getEnemyHealth();
  int enemyAttack = enemyObj.getEnemyAttack();
  int enemyDefence = enemyObj.getEnemyDefence();
+ 
+ int reduceMageDamage = mageDeffence*0.15;
+ int reduceEnemyDamage=enemyDefence*0.4;
   
  //will be used to prevent using reflect more than twice by the enemy
- int reflectCounter = 0;  
+ int reflectCounter = 0; 
+ //will be used to measure distance between enemy and the player
+ int enemyDistance = 0; 
+ // if Mage's health is below 20%, enemy automatically uses execution on him.
+ int mageExecution = mageHealth*0.2;
+ //damage of Frostbolt
+ int FrostboltDmg = mageAttack - reduceEnemyDamage; 
+ //will be used to prevent using DeepFreeze twice a row
+ int deepFreezeCounter = 0;
  
- 
- cout<<"-------------------LET THE BATTLE BEGIN!!!-------------------"<<endl; 
+  
  cout<< "You are standing in the arena. The distance between you and your \nenemy is just a few meters. "<<endl;
  cout<< "Remember only one of you will walk out of this alive."<<endl;
+ cout<<"-------------------LET THE BATTLE BEGIN!!!-------------------"<<endl;
   
   
-    Start:
-  char choice;
+  Start:
+  int randomN;
   mageObj.spells();
+  cout<< "Your health: "<<mageHealth<< "  Mana: "<< mageMana<<endl;
+  cout<< "Enemy's health: "<<enemyHealth<<endl;
+  cout<<"Choice: ";
+  char choice;
+  randomN = randomNumber();
   cin>>choice;
+  
+  
     switch (choice){
     
-  case '1': //Frostbolt
-       randomNumber();
+  case '1': //Frostbolt     
+       mageMana-=250;
+       enemyDistance++;
+       deepFreezeCounter=0;
         
-        if(enemyHealth<=0){
-          cout<<"You have successfully killed your enemy!"<<endl;
-          cout<<"Do you wish to go back to Main Menu? (Y/N): ";
-          string choice2;
-          cin>>choice2;
-          if (choice2 == "y" || choice2 == "Y"){
-            system("clear");
-            mainMenu();
-            break;
-          }
-            
-          else{
-            return 0;
-          }       
-        } // end of enemyHealth if statement
+       if(enemyDistance<2){
+          cout<<"Enemy's getting closer, you should use blink to get further away from him!"<<endl;
+        }
+        
+       else if(enemyDistance>=2){
+         cout<<"Enemy is too close to you, he swung his weapon and dealt "<< 
+           (enemyAttack-reduceMageDamage)<< " damage."<<endl;
+         mageHealth -= enemyAttack-reduceMageDamage;
+         if (mageHealth<mageExecution){
+          mageExecuted();
+          mageHealth=0;
+        }
+       }
+             
+       if (randomN>60 && reflectCounter<=1){
+         mageHealth-= FrostboltDmg;
+         cout<<"Enemy reflected your spell. You suffered "<<
+                             FrostboltDmg<<" damage\n"<<endl;
+         reflectCounter++;
+       }
+       else{
+         cout<< "Frosbolt dealt "<< FrostboltDmg<< " damage.\n"<<endl;
+         enemyHealth-=FrostboltDmg;
+       }      
+        
+       if(enemyHealth<=0){
+         enemyDefeated();
+         break;
+        }
+        
         else if(mageHealth<=0){
-          cout<<"You have been defeated by the enemy!"<<endl;
-          cout<<"Do you wish to go back to Main Menu? (Y/N): ";
-          string choice2;
-          cin>>choice2;
-          if (choice2 == "y" || choice2 == "Y"){
-            system("clear");
-            mainMenu();
-            break;
-          }
-            
-          else{
-            return 0;
-          }       
+         mageDefeated();
+          break;
         }
         
         goto Start;       
   
   case '2': //Blink
+        enemyDistance=0;
+        deepFreezeCounter=0;
+        cout<<enemyDistance<<endl;
+        mageMana-=180;
+         if (randomN>70){
+          mageHealth -= enemyAttack-reduceMageDamage;
+          cout<<"Enemy immediately charged on to you. You suffered "<<
+                             enemyAttack-reduceMageDamage<<" damage\n"<<endl;
+           if (mageHealth<mageExecution){
+          mageExecuted();
+          mageHealth=0;
+        }
+          
+         }
+        else if (randomN>30){
+          cout<< "You blinked away from the enemy and luckily casted an instant frostbolt. Damage dealt to enemy: "<<FrostboltDmg<< " . Nonetheless, act quickly"<< 
+            "'cause he is getting closer"<<endl;
+          enemyHealth -= FrostboltDmg;
+        }
+        else{
+          cout<< "You blinked away from the enemy but the fight's not over yet!"<<endl;
+        }
         
-  break;
+         if(enemyHealth<=0){
+         enemyDefeated();
+         break;
+        }
+        
+        else if(mageHealth<=0){
+         mageDefeated();
+          break;
+        
+        }
+        
+  goto Start;
   
   case '3'://DeepFreeze
+        if (deepFreezeCounter%2 == 0){
+           mageMana -= 300;
+           enemyDistance = 0;
+           deepFreezeCounter++;
+           cout<< "--------counter: "<<deepFreezeCounter<<endl;
+           cout<< "The enemy is stunned and you instantly casted Frostbolt"<<
+              " which dealt "<< FrostboltDmg<< " damage."<<endl;
+          enemyHealth -=FrostboltDmg;
+        }
+   
+         else{
+              mageHealth -= enemyAttack-reduceMageDamage;
+              cout<< "You cannot use DeepFreeze this round!"<<endl;
+              cout<<"Enemy immediately charged on to you. You suffered "<<
+                   enemyAttack-reduceMageDamage<<" damage\n"<<endl;
+              if (mageHealth<mageExecution){
+                mageExecuted();
+                mageHealth=0;
+            }
+          }
+         
+         if(enemyHealth<=0){
+          enemyDefeated();  
+           break;
+        }
         
-  break;
+        else if(mageHealth<=0){
+          mageDefeated();
+          break;
+        }
+              
+  goto Start;
   
   default:
            system("clear");
            goto Start;
     }
+  return 0;
 }
+
