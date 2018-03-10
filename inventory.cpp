8dc2ce908b9a6a7a6eda92dbaa3b3e5ed77d9c10
Harshkumar Patel (patelh28) <patelh28@coventry.ac.uk>
2018-03-10 12:57:57 +0000

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -1,66 +1,86 @@
-#include <vector>
+# include<iostream>
 #include <string>
-#include <iostream>
-#include <cstdlib>
+#include <vector>
+#include <array>
 
+using namespace std ;
 
-using namespace std;
-int main()
+int main ()
 {
-  vector<string>inventory;
-  vector<string>::iterator iter;
-  user:
-    
-  cout << "What would you like to do?" << endl;
-  cout << "1.Open Inventory \n2.Exit" << endl;
-  string User;
-    
-  cin >> User;
-  if (User == "1")
-  {
-    system("clear");
-    for(iter = inventory.begin(); iter != inventory.end(); iter++)
-    cout << *iter << "";
-      string back;
-      Back:
-      cout << endl;
-      cout << "Would you like to go back?\n1. Yes \n2. No" << endl;
-      cin >> back;
-          if (back == "1")
-          {
-              system("clear");
-              goto user;
-          }
-      else if (back == "2")
-      {
-         system("clear");
-      cout << "You have exited the program\n" << endl;
-      return 0;
-      }
-      else
-      {
-          cout << endl;
-      cout << "Invalid Input!" << endl;
-          back.clear();
-          goto Back;
-      }
+	
+	char choice;
+	int index;
+	string  number;
+	int Inventory;
+	
+	cout<<"2. Inventory \n";
+	cin>> Inventory;
+	 
+	
+	Begin:
+	if (Inventory == 2)
+	{
+		vector <string> items  ;
+		for (string i : items)
+		{cout<<i<<endl;}
+		vector <string> inventory= {"1. sword", "2. axe","3. bow","4. antidoes"} ;
+		
+		
+	  while( true)
+	  {	
+			
+	    cout<< "1. Add items \n2. Drop items \n3. Show items \n4. Exit\n" ;
+	    cin >> choice;
+		
+	    switch (choice)
+	    {
+		   case '1':					
+			
+			 
+			 cout<<"Enter item number: \n";
+			 cin>>number;
+			 inventory.push_back(number);
+				
+			
+			 break;
+			 
+			
+			 
+       case '2' :
+			
+			  
+				 cout<<"what items do you want to drop ? \n";
+			   cin>>index;
+         inventory.erase(inventory.begin()+ index);
+			   break;
+			  
+				
+		   case '3' :
+			
+			 
+				for (string i : inventory)
+		    cout<<"you picked "<<i<<endl;
+			  break;
+			 
+			
+			
+	     case '4' :
+			 
+				 return main ();
+			   break;
+			 
+					
+		   default:
+					system("clear");
+					cout<<"Not a valid option"<<endl;
+			    goto Begin;
+			 			
+	     }
+		if (inventory.size()>5)
+		{			system("clear");
+		      cout<<"Inventory Full\n";}
+	   
+    }
   }
-
-  else if (User == "2")
-    {
-      system("clear");
-      cout << "You have exited the program\n" << endl;
-      return 0;
-    }
-
-    else
-    {
-        cout << endl;
-        cout << "Invalid Input!" << endl;
-            User.clear();
-        cout << endl;
-            goto user;
-    }
- 
-  return 0;
+return 0;
 }
