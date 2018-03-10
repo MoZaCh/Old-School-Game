080e09134a0822e9c642f18b68093ae086ba6cbf
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2018-03-10 18:01:17 +0000

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -11,14 +11,14 @@ int main ()
 	char choice;
 	int index;
 	int number;
-	int Inventory;
-	
-	cout<<"2. Inventory \n";
+	string Inventory;
+	Start:
+	cout<<"1. Show Inventory Inventory \n";
 	cin>> Inventory;
 	 
 	
 	Begin:
-	if (Inventory == 2)
+	if (Inventory == "1")
 	{
 		vector <string> items = {"","sword", "axe","bow","antidoes"}  ;
    
@@ -30,24 +30,14 @@ int main ()
 	  while( true)
 	  {	
 			
-	    cout<< "1. Add items \n2. Drop items \n3. Show items \n4. Exit\n" ;
+	    cout<< "\n1. Drop items \n2. Exit\n" ;
 	    cin >> choice;
 		
 	    switch (choice)
 	    {
-		   case '1':					
 			
 			 
-			 cout<<"Enter item number: \n";
-			 cin>>number;
-			 inventory.insert(inventory.begin(),items[number]);
-				
-			
-			 break;
-			 
-			
-			 
-       case '2' :
+       case '1' :
 			
 			  
 				 cout<<"what items do you want to drop ? \n";
@@ -56,16 +46,9 @@ int main ()
 			   break;
 			  
 				
-		   case '3' :
-			
-			 
-				for (string i : inventory)
-		    cout<<"you picked "<<i<<endl;
-			  break;
-			 
 			
 			
-	     case '4' :
+	     case '2' :
 			 
 				 return main ();
 			   break;
@@ -83,5 +66,11 @@ int main ()
 	   
     }
   }
+    else
+    {
+        Inventory.clear();
+        cout << "Invalid Entry. Must be 1" << endl;
+        goto Start;
+    }
 return 0;
 }
