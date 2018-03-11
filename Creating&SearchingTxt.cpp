e820a931fbc708043ec69ac6bfb34bbb3fce47f1
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2018-03-11 00:33:48 +0000

    Update Creating&SearchingTxt.cpp

--- Creating&SearchingTxt.cpp
+++ Creating&SearchingTxt.cpp
@@ -47,3 +47,37 @@ ifstream  stream1("classChoice.txt"); //Locates File
             cout << "Hunter" << endl;
         }
         
+      
+
+      
+//Used For items
+
+      
+      
+      ofstream myfile; //Creates txt doc of class chosen
+              myfile.open ("Items.txt");
+              myfile << "Map\n";
+              myfile << "Potions x2\n";
+              myfile << "Crusty Crossbow\n";
+              myfile.close();
+                cout << endl;
+
+      
+      
+//To put the items into inventory
+      
+      
+      ifstream  stream1("Items.txt"); //Locates File
+    string line ;
+
+    while( std::getline( stream1, line ) )
+    {
+        if(line.find("Crusty Crossbow") != string::npos) // Will search for item in file
+        { 
+             //insert line into vector on inventory.
+        }
+        else if (line.find("Wand") != string::npos)// Will search for Priest in file
+        {
+           
+        }
+       
