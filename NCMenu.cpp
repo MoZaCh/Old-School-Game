30b630fd04285a47c71e1914702e6fc54e24014b
Qais Khan (khanq7) <khanq7@coventry.ac.uk>
2018-03-14 10:40:42 +0000

    Fixed Bug

--- NCMenu.cpp
+++ NCMenu.cpp
@@ -1,75 +1,60 @@
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <cstdlib>
 #include <ncurses.h>
-#include <curses.h>
-#include <panel.h>
-#include <functional>
-#include <string>
-#include "Credits.cpp"
-#include "Paths.cpp"
-
-
-
 using namespace std;
 
-int main(int argc, char ** argv)
-{ 
-    string choice[] = {"       Start New Game       ", "         Resume Game        ", "           Credits          ", "            Exit           ", };
-    int pick;
-    int highlight;
-    
-    cbreak();//command to stop the program proccess with CTRL+C
-    clear();//clears the window
 
-    /*COORDINATES OF THE FIRST MENU WINDOW*/
+int credits()
+{
+    clear();
+    system("clear");
+    
+    string choice[] = {"{Yes}","{No}"};
+    int pick;
+    int highlight = 0;
+     
+    Choice:
+      
     int height, width, start_y, start_x;
-    height = 10;
+    height = 20;
     width = 30;
     start_y = start_x = 1;
-    
-    
-    initscr();//ncurses starts
-    option:
-    WINDOW * menu_win = newwin(height, width, start_y, start_x);//creating a new window
-    refresh();//refreshes the window in a console so that changes would be seen
-    noecho();
 
     
+    initscr();
+    WINDOW * cred = newwin(height, width, start_y, start_x);
     
-    
-    box(menu_win, 0, 0);//creates a border around the window that was created
+    mvwprintw(cred, 1, 1, "Credits:");
+    mvwprintw(cred, 3, 1, "*KHAN Qais* ");
+    mvwprintw(cred, 4, 1, "*LAWAL Paul* ");
+    mvwprintw(cred, 5, 1, "*ASAVKINAS Aleksandras* ");
+    mvwprintw(cred, 6, 1, "*MAREK Peter* ");
+    mvwprintw(cred, 7, 1, "*CHOUDHURY Mohammed Zahed* ");
+    mvwprintw(cred, 8, 1, "*PATEL Harshkumar Chandrakant* ");
+    mvwprintw(cred, 9, 1, " ");
+    mvwprintw(cred, 13, 1, "Would you like to go back?: ");
+    box(cred, 0, 0);
     refresh();
-    wrefresh(menu_win);//refreshes only the menu window
-    keypad(menu_win, true);//function lets us to use the arrow keys
-    
-    
-    int y, x, yBeg, xBeg, yMax, xMax;
-    getyx(menu_win, y, x);
-    getbegyx(menu_win, yBeg, xBeg);//getting the beginning of a window
-    getmaxyx(menu_win, yMax, xMax);//getting the size of a window
-    
-    
-    mvwprintw(menu_win, 1, xMax/3, "D5 RPG Game");
-    mvwprintw(menu_win, 2, 1, "----------------------------");
+    wrefresh(cred);
+    keypad(cred, true);
     
     
     
-    title:
     
-   
-    /*CREATING A LOOP WHICH HIGLITS OUR CHOSEN CHOICE*/
+     
+     
+     wrefresh(cred);
+    
     while(true)
     {
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < 2; i++)
         {
-            if(i == highlight)
-                wattron(menu_win, A_REVERSE);
-            mvwprintw(menu_win, i+3, 1, choice[i].c_str());
-            wattroff(menu_win, A_REVERSE);
+                if(i == highlight)
+                    wattron(cred, A_REVERSE);
+                mvwprintw(cred, i+15, 1, choice[i].c_str());
+                wattroff(cred, A_REVERSE);
         }
-       pick = wgetch(menu_win);
+        pick = wgetch(cred);
         
         switch(pick)
         {
@@ -80,42 +65,32 @@ int main(int argc, char ** argv)
                 break;
             case KEY_DOWN:
                 highlight++;
-                if(highlight == 4)
-                    highlight = 3;
+                if(highlight == 2)
+                    highlight = 1;
                 break;
+            default:
+                break;      
         }
         if(pick == 10)
             break;
         
     }
     
-    /*FUNCTIONALITY OF THE CHOICES*/
-    if(choice[highlight] == "       Start New Game       ")
-    {
-            endwin(); //Leaves ncurse, goes back to normal format
-            system("clear");
-            choice[highlight] = '\0'; 
-            path();
-    }
-    else if(choice[highlight] == "         Resume Game        ")
-    {
-        
-        goto title;
-    }
-    else if(choice[highlight] == "           Credits          ")
-    {
-        endwin();
-        refresh();
-        credits();
-    }
-    else if(choice[highlight] == "            Exit           ")
+    if(choice[highlight] == "{Yes}")
     {
         endwin();
         clear();
+        system("g++ --std=c++11 NCMenu.cpp -o NCMenu -lncurses -lsqlite3 && ./NCMenu");
+    }
+    else
+    {
+        endwin();
         system("clear");
+        clear();
         return 0;
     }
-      
-    endwin();
-  return 0;
+   
+    
+     
+  endwin();
 }
