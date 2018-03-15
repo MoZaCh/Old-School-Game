03f462e705cfdff8725fb6cff7057879ac7033c2
Peter Marek (marekp) <marekp@coventry.ac.uk>
2018-03-15 19:16:50 +0000

    Update Paths.cpp

--- Paths.cpp
+++ Paths.cpp
@@ -37,6 +37,22 @@ using namespace std;
   return 0;
 }
 
+int wonCombat(){
+	  
+	  cout << "You Manage to Defeat the enemy and see the aura that was shrouding them disappear and fly away in the distance." << endl;
+    sleep(2);
+    cout << "You decide to search the persons body for anything \nYou find a not on the body. It seems to be some type of coordinate." << endl;
+    sleep(2);
+    cout << "Maybe this can can be some type of clue, but you have nothing else to go on." << endl;
+    sleep(2);
+    cout << "You can only hope this location would be the place youll be able to find the answers you need..." << endl;
+    sleep(3);
+ 
+    finish(); //Runs End Game Screen
+	
+    return 0;
+  }
+
 int path()
 {
 	
@@ -720,17 +736,6 @@ cout << "You seem to not be able to remember much but you do gather your name." 
     cout << endl;
     
     combat(); // Runs Combat
-    
-    cout << "You Manage to Defeat the enemy and see the aura that was shrouding them disappear and fly away in the distance." << endl;
-    sleep(2);
-    cout << "You decide to search the persons body for anything \nYou find a not on the body. It seems to be some type of coordinate." << endl;
-    sleep(2);
-    cout << "Maybe this can can be some type of clue, but you have nothing else to go on." << endl;
-    sleep(2);
-    cout << "You can only hope this location would be the place youll be able to find the answers you need..." << endl;
-    sleep(3);
- 
-    finish(); //Runs End Game Screen
-    
+  
         return 0;
 }
