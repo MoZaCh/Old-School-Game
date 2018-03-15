6cd972ce8438ad4d9b62eff42fc0b604b4437c38
Peter Marek (marekp) <marekp@coventry.ac.uk>
2018-03-15 23:30:21 +0000

    Update CombatEnding.cpp

--- CombatEnding.cpp
+++ CombatEnding.cpp
@@ -9,22 +9,24 @@ using namespace std;
 int combatEnding(){
 	  
 	  cout << "You Manage to Defeat the enemy and see the aura that was shrouding them disappear and fly away in the distance." << endl;
- //   sleep(2);
+   sleep(2);
     cout << "You decide to search the persons body for anything \nYou find a not on the body. It seems to be some type of coordinate." << endl;
-  //  sleep(2);
+    sleep(2);
     cout << "Maybe this can can be some type of clue, but you have nothing else to go on." << endl;
-   // sleep(2);
+    sleep(2);
     cout << "You can only hope this location would be the place youll be able to find the answers you need..." << endl;
-    //sleep(3);
+    sleep(3);
  string choice;
   
- cout<< "w: ";
+ cout<< "Would you like to see the credits for this game(Y/N)?: ";
  cin>>choice;
   
- if (choice == "y"){
-   finish(); //Runs End Game Screen
-   
+ if (choice == "y" || choice == "Y"){
+   finish(); //Runs End Game Screen  
  }
+  else{
+    exit(0);
+  }
     
 	
     return 0;
