0af11556eed7e7fcb59aad167e86f3934efab5cc
Peter Marek (marekp) <marekp@coventry.ac.uk>
2018-03-15 20:24:59 +0000

    Create CombatEnding.cpp

---
+++ CombatEnding.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ncurses.h>
+#include "Finish.cpp"
+#pragma once
+using namespace std;
+
+int combatEnding(){
+	  
+	  cout << "You Manage to Defeat the enemy and see the aura that was shrouding them disappear and fly away in the distance." << endl;
+ //   sleep(2);
+    cout << "You decide to search the persons body for anything \nYou find a not on the body. It seems to be some type of coordinate." << endl;
+  //  sleep(2);
+    cout << "Maybe this can can be some type of clue, but you have nothing else to go on." << endl;
+   // sleep(2);
+    cout << "You can only hope this location would be the place youll be able to find the answers you need..." << endl;
+    //sleep(3);
+ string choice;
+  
+ cout<< "w: ";
+ cin>>choice;
+  
+ if (choice == "y"){
+   finish(); //Runs End Game Screen
+   
+ }
+    
+	
+    return 0;
+  }
